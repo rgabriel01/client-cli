8b66d0b1f97d620b8dc4d50d52d71ae1871fd69c
rgabriel01 <raymond.m.gabriel@gmail.com>
2025-04-10 13:03:25 +0800

    adds rspec gem, configuration and its initial implementation

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@
 source "https://rubygems.org"
 
 gem 'byebug'
+gem "rspec", "~> 3.12"
---
+++ spec/search_spec.rb
@@ -0,0 +1,11 @@
+# rspec spec/search_spec.rb
+
+require_relative '../lib/search'
+
+RSpec.describe Search do
+  describe '#search' do
+    it 'passes' do
+      expect(true).to be_truthy
+    end
+  end
+end
